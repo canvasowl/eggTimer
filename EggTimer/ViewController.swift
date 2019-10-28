53d0580f081dae15770104d31116af742d6c9aa3
Jefry Cruz <jefrycruz88@gmail.com>
2019-10-27 23:19:36 -0400

    timer works

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    @IBOutlet weak var mainLabel: UILabel!
+    let eggTimes = [
+        // in seconds
+        "Soft": 3,
+        "Medium": 4,
+        "Hard": 7
+    ]
+    var remainingSeconds = 0
+    var timer = Timer()
+    
+    // handles the eggs being pressed
+    @IBAction func eggPressed(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        timer.invalidate()
+        remainingSeconds = eggTimes[hardness]!
+        startTimer(seconds: eggTimes[hardness]!)
+    }
+    
+    // start the timer
+    fileprivate func startTimer(seconds: Int) -> Timer {
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timerCallback), userInfo: nil, repeats: true)
+        
+        return timer
+    }
+    
+    // update the timer time
+    @objc func timerCallback(){
+        if remainingSeconds > 0 {
+            print("\(remainingSeconds) seconds.")
+            remainingSeconds -= 1
+        }else{
+            timer.invalidate()
+            mainLabel.text = "DONE!"
+        }
+    }
+    
 }
